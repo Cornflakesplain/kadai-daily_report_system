95c4c8bde22082af8625270ce3c1d428774048a3
jun.tamura <cornflakesplain_0310@ezweb.ne.jp>
2018-06-13 23:51:58 +0900

    fixed resource

--- src/controllers/reports/ReportsIndexServlet.java
+++ src/controllers/reports/ReportsIndexServlet.java
@@ -1,9 +1,7 @@
 package controllers.reports;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,10 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Report;
-import utils.DBUtil;
+import execute.ReportExecute;
 import utils.PropertyUtils;
-import utils.ServletUtils;
 
 /**
  * Servlet implementation class ReportsIndexServlet
@@ -36,40 +32,7 @@ public class ReportsIndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//	    ReportExecute.doIndex(request, response);
-	    
-        // 現在のページを取得
-        int page;
-        try {
-            page = Integer.parseInt(request.getParameter("page"));
-        } catch(Exception e) {
-            page = 1;
-        }
-        
-        // 全体のページ数を取得
-        EntityManager em = DBUtil.createEntityManager();
-        List<Report> reports = em.createNamedQuery(PropertyUtils.QRY_GET_ALL_REPORTS, Report.class)
-                .getResultList();
-        
-        response.getWriter().append(Integer.valueOf(reports.size()).toString());
-
-
-//        List<Report> reports = em.createNamedQuery(PropertyUtils.QRY_GET_ALL_REPORTS, Report.class)
-//                                    .setFirstResult(15 * (page - 1))
-//                                    .setMaxResults(15)
-//                                    .getResultList();
-//        // 日誌の総数を取得
-//        long reports_count = (long)em.createNamedQuery(PropertyUtils.QRY_GET_REPORTS_COUNT,Long.class)
-//                                    .getSingleResult();
-        
-        em.close();
-        
-        request.setAttribute("reports", reports);
-//        request.setAttribute("reports_count", reports_count);
-        request.setAttribute("page", page);
-        
-        // Flushメッセージが存在する場合は取得
-        ServletUtils.existsThenSetFlush(request);
+	    ReportExecute.doIndex(request, response);
 
 	    RequestDispatcher rd = request.getRequestDispatcher(PropertyUtils.FORWARD_REPORTS_INDEX);
 	    rd.forward(request, response);
